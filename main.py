36369510ad355a96b7431bea17391414fcfb3335
zakia <zakialounas4@gmail.com>
2026-08-19 21:11:00 +0200

    Afficher les informations générales des données dvf 2021

--- main.py
+++ main.py
@@ -11,5 +11,20 @@ if __name__== '__main__':
     df2022= lire_fichier("data\\dvf\\ValeursFoncieres-2022.txt")
     df2023= lire_fichier("data\\dvf\\ValeursFoncieres-2023.txt")
     df2024= lire_fichier("data\\dvf\\ValeursFoncieres-2024.txt")
-    df2026= lire_fichier("data\\dvf\\ValeursFoncieres-2025.txt")
+    df2025= lire_fichier("data\\dvf\\ValeursFoncieres-2025.txt")
 
+    print("Données 2021 : \n")
+
+    print("Nombre de lignes : ",df2021.shape[0])
+    print("Nombre de colonnes : ",df2021.shape[1])
+
+    print("Colonnes : ", df2021.columns)
+    print("Types des colonnes : ", df2021.dtypes)
+
+    print("Valeur nulles : \n", df2021.isnull().sum())
+
+    print("\nInformations générales : ")
+    df2021.info();
+
+    print("Les 5 premières lignes : ")
+    print(df2021.head())

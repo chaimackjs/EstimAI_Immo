f4a33d7fbbebe1187cbe0964212ea50b91ed05fd
zakia <zakialounas4@gmail.com>
2026-08-19 20:47:58 +0200

    Ajouter la lecture des5 fichiers dvf avec pandas

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print ("bonjour")
+import pandas as pd 
+
+def lire_fichier(chemin): 
+    return pd.read_csv(chemin,sep="|")
+
+
+
+if __name__== '__main__':
+
+    df2021= lire_fichier("data\\dvf\\ValeursFoncieres-2021.txt")
+    df2022= lire_fichier("data\\dvf\\ValeursFoncieres-2022.txt")
+    df2023= lire_fichier("data\\dvf\\ValeursFoncieres-2023.txt")
+    df2024= lire_fichier("data\\dvf\\ValeursFoncieres-2024.txt")
+    df2026= lire_fichier("data\\dvf\\ValeursFoncieres-2025.txt")
+
